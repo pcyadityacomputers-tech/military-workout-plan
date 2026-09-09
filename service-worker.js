b95152ae0f209851d68ee401beabc83ee8a6fb05
pcyadityacomputers-tech <pcy.adityacomputers@gmail.com>
2026-09-09 06:14:42 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "milfit-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon.svg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
